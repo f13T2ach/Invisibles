9866645bfcd8b8a7b15c63a2ed34b8f42ffa4216
Sokasne Redmaple <101922505+f13T2ach@users.noreply.github.com>
2022-12-25 13:03:24 +0800

    Update Update.php

--- LipService/Update.php
+++ LipService/Update.php
@@ -1,1 +1,1 @@
-1.9,3577499,7b73bbd4bc60cca13d1a7fe5c2bce966,https://raw.githubusercontent.com/f13T2ach/Invisibles/main/LipService/Release.apk
+1.3,3577499,7b73bbd4bc60cca13d1a7fe5c2bce966,https://raw.githubusercontent.com/f13T2ach/Invisibles/main/LipService/Release.apk
